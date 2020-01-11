839a8c6c71517eea89c269a0784306123ff3a1a0
Abu Ali Muhammad Sharjeel <sheryever@hotmail.com>
2020-01-12 01:17:51 +0300

    KeyAttribute added for PrimaryKey

--- Source/SimpleAccess.Core/Attributes/IdentityAttribute.cs
+++ Source/SimpleAccess.Core/Attributes/IdentityAttribute.cs
@@ -6,7 +6,15 @@ namespace SimpleAccess
     /// Mark a Entity property as Identity columns
     /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
-    public class IdentityAttribute : Attribute
+    public class IdentityAttribute : KeyAttribute
     {
+        public string DbSequence { get; }
+
+        public IdentityAttribute() { }
+
+        public IdentityAttribute(string dbSequence)
+        {
+            DbSequence = dbSequence;
+        }
     }
 }
---
+++ Source/SimpleAccess.Core/Attributes/KeyAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleAccess
+{
+    /// <summary>
+    /// Mark a Entity property as Identity columns
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class KeyAttribute : Attribute
+    {
+    }
+}
