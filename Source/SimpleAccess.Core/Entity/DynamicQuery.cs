12be9353ef95a54dbf8822adb78c849241c3b66a
Abu Ali Muhammad Sharjeel <sheryever@hotmail.com>
2020-07-06 01:14:41 +0300

    Support creating having clause added

--- Source/SimpleAccess.Core/Entity/DynamicQuery.cs
+++ Source/SimpleAccess.Core/Entity/DynamicQuery.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Data;
 using System.Dynamic;
 using System.Linq;
@@ -137,11 +138,13 @@ namespace SimpleAccess.Core.Entity
 
                 builder.Append(" WHERE ");
 
+                var entityType = typeof(TEntitiy);
+                
                 for (int i = 0; i < queryProperties.Count(); i++)
                 {
                     QueryParameter item = queryProperties[i];
 
-                    var propertyInfo = typeof(TEntitiy).GetProperty(item.PropertyName);
+                    var propertyInfo = entityType.GetProperty(item.PropertyName);
                     if (!string.IsNullOrEmpty(item.LinkingOperator) && i > 0)
                     {
                         //builder.Append(string.Format("{0} {1} {2} @{1} ", item.LinkingOperator, item.PropertyName,
@@ -161,7 +164,72 @@ namespace SimpleAccess.Core.Entity
 
                 return builder.ToString();
             }
-            
+
+
+            /// <summary>
+            /// Gets the dynamic query.
+            /// </summary>
+            /// <param name="tableName">Name of the table.</param>
+            /// <param name="expression">The expression.</param>
+            /// <returns>A result object with the generated sql and dynamic params.</returns>
+            public static string CreateDbParametersFormHavingExpression<TISqlBuilder, TDbParameter, TEntitiy>(Expression<Func<TEntitiy, bool>> expression, EntityInfo<TISqlBuilder, TDbParameter> entityInfo)
+                where TISqlBuilder : ISqlBuilder<TDbParameter>, new()
+                where TDbParameter : IDataParameter
+            {
+                BinaryExpression binaryExpressionBody = null;
+                MethodCallExpression methodCallExpressionBody = null;
+                var queryProperties = new List<QueryHavingParameter>();
+                if (expression.Body is BinaryExpression)
+                {
+                    binaryExpressionBody = (BinaryExpression)expression.Body;
+                    // walk the tree and build up a list of query parameter objects
+                    // from the left and right branches of the expression tree
+                    WalkTreeHavingExpressoin(binaryExpressionBody, ExpressionType.Default, ref queryProperties);
+                }
+                else if (expression.Body is MethodCallExpression)
+                {
+                    methodCallExpressionBody = (MethodCallExpression)expression.Body;
+                    // walk the tree and build up a list of query parameter objects
+                    // from the left and right branches of the expression tree
+                    WalkTreeHavingExpressoin(methodCallExpressionBody, ExpressionType.Default, ref queryProperties);
+
+                }
+
+                IDictionary<string, Object> expando = new ExpandoObject();
+                var builder = new StringBuilder();
+
+                // convert the query parms into a SQL string and dynamic property object
+
+                builder.Append(" HAVING ");
+
+                var entityType = typeof(TEntitiy).GetGenericArguments()[0];
+
+                for (int i = 0; i < queryProperties.Count(); i++)
+                {
+                    QueryHavingParameter item = queryProperties[i];
+
+                    var propertyInfo = entityType.GetProperty(item.PropertyName);
+                    if (!string.IsNullOrEmpty(item.LinkingOperator) && i > 0)
+                    {
+                        //builder.Append(string.Format("{0} {1} {2} @{1} ", item.LinkingOperator, item.PropertyName,
+                        builder.Append(string.Format("{0}  {1} ", item.LinkingOperator,
+                            entityInfo.SqlBuilder.BuildWhereExpression($"{item.FunctionName}({item.PropertyName})", propertyInfo.PropertyType,
+                                item.QueryOperator, item.PropertyValue)));
+
+                    }
+                    else
+                    {
+                        builder.Append(entityInfo.SqlBuilder.BuildWhereExpression($"{item.FunctionName}({item.PropertyName})", propertyInfo.PropertyType,
+                                    item.QueryOperator, item.PropertyValue));
+                        //                                entityInfo.SqlBuilder.BuildValueOperand(propertyInfo.PropertyType, item.PropertyValue)));
+
+                    }
+                }
+
+                return builder.ToString();
+            }
+
+
             /// <summary>
             /// Walks the tree.
             /// </summary>
@@ -208,6 +276,51 @@ namespace SimpleAccess.Core.Entity
             /// <param name="body">The body.</param>
             /// <param name="linkingType">Type of the linking.</param>
             /// <param name="queryProperties">The query properties.</param>
+            private static void WalkTreeHavingExpressoin(BinaryExpression body, ExpressionType linkingType,
+                                         ref List<QueryHavingParameter> queryProperties)
+            {
+                if (body.NodeType != ExpressionType.AndAlso && body.NodeType != ExpressionType.OrElse)
+                {
+                    dynamic aggregateFuncAndColumn = GetHavingFunctionAndColumn(body);
+                    dynamic propertyValue = GetExpressionHavingValue(body);
+                    if (propertyValue.GetType().IsGenericType)
+                    {
+                        propertyValue = $"{propertyValue.FunctionName}({propertyValue.ColumnName})";
+                    }
+                    //dynamic propertyValue = GetExpressionValue(body);
+                    string opr = GetOperator(body.NodeType);
+                    string link = GetOperator(linkingType);
+
+                    queryProperties.Add(new QueryHavingParameter(link, aggregateFuncAndColumn.FunctionName, aggregateFuncAndColumn.ColumnName, propertyValue, opr));
+                }
+                else
+                {
+                    if (body.Left is BinaryExpression)
+                    {
+                        WalkTreeHavingExpressoin((BinaryExpression)body.Left, body.NodeType, ref queryProperties);
+                    }
+                    else if (body.Left is MethodCallExpression)
+                    {
+                        WalkTreeHavingExpressoin((MethodCallExpression)body.Left, body.NodeType, ref queryProperties);
+                    }
+                    if (body.Right is BinaryExpression)
+                    {
+                        WalkTreeHavingExpressoin((BinaryExpression)body.Right, body.NodeType, ref queryProperties);
+                    }
+                    else if (body.Right is MethodCallExpression)
+                    {
+                        WalkTreeHavingExpressoin((MethodCallExpression)body.Right, body.NodeType, ref queryProperties);
+                    }
+                }
+            }
+
+
+            /// <summary>
+            /// Walks the tree.
+            /// </summary>
+            /// <param name="body">The body.</param>
+            /// <param name="linkingType">Type of the linking.</param>
+            /// <param name="queryProperties">The query properties.</param>
             private static void WalkTree(MethodCallExpression body, ExpressionType linkingType,
                                          ref List<QueryParameter> queryProperties)
             {
@@ -218,6 +331,24 @@ namespace SimpleAccess.Core.Entity
 
                 queryProperties.Add(new QueryParameter(link, propertyName, propertyValue, opr));
             }
+
+            /// <summary>
+            /// Walks the tree.
+            /// </summary>
+            /// <param name="body">The body.</param>
+            /// <param name="linkingType">Type of the linking.</param>
+            /// <param name="queryProperties">The query properties.</param>
+            private static void WalkTreeHavingExpressoin(MethodCallExpression body, ExpressionType linkingType,
+                                         ref List<QueryHavingParameter> queryProperties)
+            {
+                dynamic aggregateFuncAndColumn = GetHavingFunctionAndColumn(body);
+                dynamic propertyValue = GetExpressionHavingValue(body);
+                string opr = body.Method.Name;
+                string link = GetOperator(linkingType);
+
+                queryProperties.Add(new QueryHavingParameter(link, aggregateFuncAndColumn.FunctionName, aggregateFuncAndColumn.ColumnName, propertyValue, opr));
+            }
+
 
             /// <summary>
             /// Gets the name of the property.
@@ -235,6 +366,29 @@ namespace SimpleAccess.Core.Entity
                 }
 
                 return propertyName;
+            }
+
+            /// <summary>
+            /// Gets the name of the property.
+            /// </summary>
+            /// <param name="body">The body.</param>
+            /// <returns>The property name for the property expression.</returns>
+            private static dynamic GetHavingFunctionAndColumn(BinaryExpression body)
+            {
+                // cut the bullshit
+                var values = body.Left.ToString().Split(new char[] { '.' });
+
+                var function = values[1].Substring(0, values[1].IndexOf("("));
+                var column = values[2].Substring(0, values[2].IndexOf(")"));
+
+
+                //if (body.Left.NodeType == ExpressionType.Convert)
+                //{
+                //    // hack to remove the trailing ) when convering.
+                //    propertyName = propertyName.Replace(")", string.Empty);
+                //}
+
+                return new { FunctionName = function, ColumnName = column };
             }
 
             private static object GetExpressionValue(MethodCallExpression body)
@@ -265,12 +419,58 @@ namespace SimpleAccess.Core.Entity
                 }
             }
 
+            private static object GetExpressionHavingValue(MethodCallExpression body)
+            {
+                if (body.Arguments[0] is ConstantExpression)
+                {
+                    return ((dynamic)body.Arguments[0]).Value;
+                }
+                else
+                {
+                    LambdaExpression lambda = Expression.Lambda(body.Arguments[0]);
+                    var compiledExpression = lambda.Compile();
+                    return compiledExpression.DynamicInvoke();
+                }
+            }
+
+            private static object GetExpressionHavingValue(BinaryExpression body)
+            {
+                if (body.Right is ConstantExpression)
+                {
+                    return ((dynamic)body.Right).Value;
+                }
+                // Beleive me don't have time for this
+                else if(body.Right.GetType().Name.IndexOf("InstanceMethodCallExpression") > -1)
+                {
+                    return GetHavingFunctionAndColumn(body);
+                }
+                else
+                {
+                    LambdaExpression lambda = Expression.Lambda(body.Right);
+                    var compiledExpression = lambda.Compile();
+                    return compiledExpression.DynamicInvoke();
+                }
+            }
+
             /// <summary>
             /// Gets the name of the property.
             /// </summary>
             /// <param name="body">The body.</param>
             /// <returns>The property name for the property expression.</returns>
             private static string GetPropertyName(MethodCallExpression body)
+            {
+                string propertyName = body.Object.ToString().Split(new char[] { '.' })[1];
+
+                return propertyName;
+            }
+
+
+            /// <summary>
+            /// Gets the name of the property.
+            /// </summary>
+            /// <param name="body">The body.</param>
+            /// <returns>The property name for the property expression.</returns>
+            private static string GetHavingFunctionAndColumn(MethodCallExpression body)
             {
                 string propertyName = body.Object.ToString().Split(new char[] { '.' })[1];
 
@@ -360,6 +560,11 @@ namespace SimpleAccess.Core.Entity
                 return expression as MethodCallExpression;
             }
         }
+
+        internal enum ClauseType
+        {
+            Where, Having
+        }
 #endregion
 
         /// <summary>
@@ -388,7 +593,33 @@ namespace SimpleAccess.Core.Entity
             }
         }
 
+        /// <summary>
+        /// Class that models the data structure in coverting the expression tree into SQL Having and Params.
+        /// </summary>
+        internal class QueryHavingParameter
+        {
+            public string LinkingOperator { get; set; }
+            public string FunctionName { get; set; }
+            public string PropertyName { get; set; }
+            public object PropertyValue { get; set; }
+            public string QueryOperator { get; set; }
 
+            /// <summary>
+            /// Initializes a new instance of the <see cref="QueryParameter" /> class.
+            /// </summary>
+            /// <param name="linkingOperator">The linking operator.</param>
+            /// <param name="propertyName">Name of the property.</param>
+            /// <param name="propertyValue">The property value.</param>
+            /// <param name="queryOperator">The query operator.</param>
+            internal QueryHavingParameter(string linkingOperator, string functionName, string propertyName, object propertyValue, string queryOperator)
+            {
+                this.LinkingOperator = linkingOperator;
+                this.FunctionName = functionName;
+                this.PropertyName = propertyName;
+                this.PropertyValue = propertyValue;
+                this.QueryOperator = queryOperator;
+            }
+        }
     }
 
 }
